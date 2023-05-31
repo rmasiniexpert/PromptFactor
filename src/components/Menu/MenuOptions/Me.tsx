7e1747d95e9547f35b467b12bfcd3df6eba82a31
Rodrigo Masini <rmasiniexpert@gmail.com>
2023-05-31 17:25:23 -0400

    Update Me.tsx

--- src/components/Menu/MenuOptions/Me.tsx
+++ src/components/Menu/MenuOptions/Me.tsx
@@ -4,11 +4,10 @@ import { useTranslation } from 'react-i18next';
 import HeartIcon from '@icon/HeartIcon';
 
 const Me = () => {
-  const { t } = useTranslation();
   return (
     <a
       className='flex py-2 px-2 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-      href='https://github.com/ztjhz/BetterChatGPT'
+      href='https://github.com/rmasiniexpert'
       target='_blank'
     >
       <HeartIcon />
